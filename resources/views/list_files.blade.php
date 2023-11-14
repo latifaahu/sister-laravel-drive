3610b70dcfeecdb1383ea8f8c2cd35a0e83e7601
Bintang Putro <bntngputro@gmail.com>
2023-11-14 18:51:13 +0700

    enhance the visual

--- resources/views/list_files.blade.php
+++ resources/views/list_files.blade.php
@@ -4,26 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>List of Files</title>
+    <!-- Add Bootstrap CDN -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    <h1>List of Files in the "Project" Folder</h1>
+<body class="container mt-5">
+    <h1 class="mb-4">List of Files in the "Project" Folder</h1>
     @if (session('success'))
-    <p style="color: green;">{{ session('success') }}</p>
+        <p class="alert alert-success">{{ session('success') }}</p>
     @endif
     @if (isset($error))
-        <p style="color: red;">{{ $error }}</p>
+        <p class="alert alert-danger">{{ $error }}</p>
     @else
         <form action="{{ route('google.drive.delete') }}" method="post">
             @csrf
             @method('DELETE')
             @if (count($files) > 0)
-                <table border="1">
+                <table class="table">
                     <thead>
                         <tr>
                             <th>ID</th>
                             <th>Description</th>
                             <th>Name</th>
-                            <th>Delete</th>
+                            <th>Pilih File Delete</th>
+                            <th>Update</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -37,23 +40,22 @@
                                     </a>
                                 </td>
                                 <td>
-                                    <input type="checkbox" name="file_ids[]" value="{{ $file->id }}">
+                                    <input type="checkbox" class="form-check-input" name="file_ids[]" value="{{ $file->id }}">
                                 </td>
                                 <td>
-                                    <a href="{{ url('google/drive/update', ['driveId' => $file->id]) }}">Update</a>
+                                    <a href="{{ url('google/drive/update', ['driveId' => $file->id]) }}" class="btn btn-warning">Update</a>
                                 </td>                                                                                 
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
-                <button type="submit">Delete Selected Files</button>
+                <button type="submit" class="btn btn-danger">Delete Selected Files</button>
             @else
-                <p>No files found in the "Project" folder.</p>
+                <p class="alert alert-info">No files found in the "Project" folder.</p>
             @endif
         </form>
         <br>
-        <!-- Add a button to navigate to the file upload form -->
-        <a href="{{ route('google.drive.upload.form') }}">Go to File Upload Form</a>
+        <a href="{{ route('google.drive.upload.form') }}" class="btn btn-primary">Go to File Upload Form</a>
     @endif
 </body>
 </html>
--- resources/views/update_file.blade.php
+++ resources/views/update_file.blade.php
@@ -4,25 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update File</title>
+    <!-- Add Bootstrap CDN -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    <h1>Update File</h1>
+<body class="container mt-5">
+    <h1 class="mb-4">Update File</h1>
     @if (isset($error))
-        <p style="color: red;">{{ $error }}</p>
+        <p class="alert alert-danger">{{ $error }}</p>
     @else
-    <form action="{{ route('google.drive.update', ['driveId' => $file->google_drive_id]) }}" method="post" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
-        <label for="description">Description:</label>
-        <input type="text" name="description" value="{{ $file->description }}">
+        <form action="{{ route('google.drive.update', ['driveId' => $file->google_drive_id]) }}" method="post" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="description">Description:</label>
+                <input type="text" class="form-control" name="description" value="{{ $file->description }}">
+            </div>
+            <div class="form-group">
+                <label for="file">Choose a new file:</label>
+                <input type="file" class="form-control-file" name="file">
+            </div>
+            <button type="submit" class="btn btn-success">Update File</button>
+        </form>
         <br>
-        <label for="file">Choose a new file:</label>
-        <input type="file" name="file">
-        <br>
-        <button type="submit">Update File</button>
-    </form>
-    <br>
-    <a href="{{ route('google.drive.list') }}">Back to List</a>    
+        <a href="{{ route('google.drive.list') }}" class="btn btn-primary">Back to List</a>    
     @endif
 </body>
 </html>
